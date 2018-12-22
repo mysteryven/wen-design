7ecf314d98a688503364247462da05dc0f5b6671
wenzhe <mysteryven@gmail.com>
2018-12-22 22:06:47 +0800

    set up environment, add parcel, separate button by vue component

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('z-button', Button)
+
+let app = new Vue({
+    el: '#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="z-button">按钮</button>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .z-button {
+        cursor: pointer;
+        font-size: var(--font-size);
+        padding: 0 1em;
+        border: 1px solid var(--border-color);
+        height: var(--button-height);
+        background: var(--button-bg);
+        border-radius: var(--button-border-radius);
+        outline: none;
+    }
+    .z-button:hover {
+        border: 1px solid var(--border-color-hover);
+        box-shadow: 1px 1px 10px rgba(0, 0, 0,0.1);
+    }
+    .z-button:active {
+        background: var(--button-active-bg);
+    }
+</style>
