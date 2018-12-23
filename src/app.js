6527b9550f7a0305c805c339d672edf869cd516f
wenzhe <mysteryven@gmail.com>
2018-12-23 11:42:54 +0800

    create button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('z-button', Button)
 Vue.component('z-icon', Icon)
+Vue.component('z-button-group', ButtonGroup)
 
 let app = new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="z-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style lang="scss">
+    .z-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .z-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-bottom-left-radius: var(--button-border-radius);
+                border-top-left-radius: var(--button-border-radius);
+                margin-left: 0px;
+            }
+            &:last-child {
+                border-top-right-radius: var(--button-border-radius);
+                border-bottom-right-radius: var(--button-border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+
+    }
+</style>
