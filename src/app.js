d4b00fb3048d41fa4d457062fdbf566f656c789d
wenzhe <mysteryven@gmail.com>
2018-12-23 14:02:56 +0800

    add button select

--- src/app.js
+++ src/app.js
@@ -2,16 +2,18 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import ButtonSelect from './button-select'
 
 Vue.component('z-button', Button)
 Vue.component('z-icon', Icon)
 Vue.component('z-button-group', ButtonGroup)
+Vue.component('z-button-select', ButtonSelect)
 
 let app = new Vue({
     el: '#app',
     data: {
         loading: false,
         disabled: true,
-        selectList: ['apple', 'banana', 'orange']
+        selectList: ['苹果', '香蕉', '橘子']
     }
 })
---
+++ src/button-select.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="z-button-select">
+        <z-button icon="down" icon-position="right" @click="isShow = !isShow">{{currentItem}}</z-button>
+        <ul v-if="isShow">
+            <li v-for="item in list" @click="setItem(item)">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['list'],
+        data: function () {
+            return {
+                currentItem: '',
+                isShow: false
+            }
+        },
+        created() {
+            this.currentItem = this.list[0]
+        },
+        methods: {
+            setItem(newValue){
+                this.currentItem = newValue
+                this.isShow = false
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    ul, li {margin: 0; list-style: none; padding: 0;}
+    .z-button-select {
+        position: relative;
+        ul {
+            position: absolute;
+            padding-top: 6px;
+            top: 130%;
+            left: 0px;
+            box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
+            font-size: var(--font-size);
+            border-radius: var(--button-border-radius);
+            background: white;
+            &::before {
+                content: '';
+                position: absolute;
+                background-color: white;
+                width: 8px;
+                height: 8px;
+                box-shadow: 0 0 1px rgba(0, 0, 0, 0.3);
+                transform: rotate(45deg);
+                top: -4%;
+                left: 16%;
+            }
+            &::after {
+                content: '';
+                width: 12px;
+                height: 7px;
+                position: absolute;
+                background-color: white;
+                top: 0%;
+                left: 14%;
+                z-index: 2;
+            }
+            li {
+                padding: 0.2em 4em 0.5em 1em;
+                &:hover {
+                    background: var(--button-active-bg);
+                }
+            }
+        }
+    }
+</style>
