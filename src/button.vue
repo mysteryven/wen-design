7d48c9eb18e101b4a284902cb0faa3e1083466de
wenzhe <mysteryven@gmail.com>
2018-12-23 10:38:14 +0800

    import svg, add icon position and validator

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,38 @@
 <template>
-    <button class="z-button">按钮</button>
+    <button class="z-button" :class="{[`icon-${iconPosition}`]: true}" >
+        <svg class="icon" v-if="icon" aria-hidden="true">
+            <use :xlink:href="`#icon-${icon}`"></use>
+            <use :xlink:href="`#icon-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+        </button>
 </template>
 <script>
     export default {
-
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(newValue) {
+                    return !(newValue !== 'left' && newValue !== 'right');
+                }
+            }
+        }
     }
 </script>
 
 <style lang="scss">
+    .icon {
+        width: 1em;
+        height: 1em;
+    }
     .z-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         cursor: pointer;
         font-size: var(--font-size);
         padding: 0 1em;
@@ -17,12 +41,32 @@
         background: var(--button-bg);
         border-radius: var(--button-border-radius);
         outline: none;
+        vertical-align: middle;
+        &:hover {
+            border: 1px solid var(--border-color-hover);
+            box-shadow: 1px 1px 10px rgba(0, 0, 0,0.1);
+        }
+        &:active{
+            background: var(--button-active-bg);
+        }
+        >.icon {
+            order: 1;
+            margin-right: 0.3em;
+            margin-left: 0
+        }
+        >.content {
+            order: 2
+        }
+        &.icon-right {
+            >.icon {
+                order: 2;
+                margin-left: 0.3em;
+                margin-right: 0
+            }
+            >.content {
+                order: 1
+            }
+        }
     }
-    .z-button:hover {
-        border: 1px solid var(--border-color-hover);
-        box-shadow: 1px 1px 10px rgba(0, 0, 0,0.1);
-    }
-    .z-button:active {
-        background: var(--button-active-bg);
-    }
+
 </style>
