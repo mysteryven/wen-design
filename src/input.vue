b173b4a5066e5b4365c5530379d03859f80b6b99
wenzhe <mysteryven@gmail.com>
2018-12-27 20:52:40 +0800

    添加 错误信息的图标，添加input提示信息的两种位置选择

--- src/input.vue
+++ src/input.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper" :class="{bottom: errorPosition === 'bottom'}">
         <input type="text" :placeholder="placeholder">
+        <div class="error" v-if="error">
+            <z-icon name="error" class="error-icon"></z-icon>
+            <span>{{error}}</span>
+        </div>
     </div>
 </template>
 <script>
+
+    import  Icon from './icon'
     export default {
         props: {
             placeholder: {
                 default: '提示信息',
                 type: String
+            },
+            error: {
+                default: '',
+                type: String
+            },
+            'errorPosition': {
+                default: 'right',
+                type: String,
+                validator(newValue) {
+                    console.log(newValue)
+                    return !(newValue !== 'bottom' && newValue !== 'right');
+                }
             }
+        },
+        components: {
+            'z-icon': Icon
         }
     }
 </script>
@@ -19,8 +40,27 @@
     $border-color-hover: #666;
     $font-size: 14px;
     $border-radius: 4px;
+    .wrapper {
+        display: inline-flex;
+        align-items: center;
+        .error {
+            display: inline-flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: flex-start;
+        }
+    }
+    .wrapper.bottom {
+        display: inline-flex;
+        flex-direction: column;
+        align-items: flex-start;
+        .error-icon {
+            margin-left: 0;
+        }
+    }
     .wrapper input {
         height: $height;
+        display: inline-flex;
         font-size: $font-size;
         border: 1px solid $border-color;
         outline: none;
@@ -30,7 +70,25 @@
         &:active {
             box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
         }
+        &:focus {
+            box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.2);
+        }
         border-radius: $border-radius;
         padding: 1em 0.5em;
     }
+    .error {
+        display: inline-flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+    .error-icon {
+        fill: red;
+        margin: 0 0.2em 0 0.5em;
+        vertical-align: middle;
+    }
+    span {
+        color: red;
+        vertical-align: middle;
+    }
+
 </style>
