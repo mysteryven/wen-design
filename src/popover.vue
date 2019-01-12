8f8d5f50bb04fda0094a68624e80996f78965498
wenzhe <mysteryven@gmail.com>
2019-01-12 23:51:29 +0800

    为popover添加点击 focus hover 三种状态

--- src/popover.vue
+++ src/popover.vue
@@ -13,27 +13,78 @@
         name: 'zPopover',
         data() {
            return {
-               visible: true
+               visible: false
            }
         },
-        mounted() {
-            this.$refs.trigger.addEventListener('click', () => {
-                this.onClick()
-            })
-            document.body.addEventListener('click', (e) => {
-                let {trigger, content, popover} = this.$refs
-                if (this.$refs && (trigger.contains(e.target) || trigger === e.target)) {
-                    console.log(1)
-                    return
+        props: {
+            trigger: {
+                type: String,
+                default: 'click',
+                validator(value) {
+                   return ['click', 'hover', 'focus'].indexOf(value) !== -1
                 }
-                console.log('document be clicked')
-            })
+            }
+        },
+        computed: {
+
+        },
+        mounted() {
+            if (this.trigger === 'click') {
+                this.$refs.trigger.addEventListener('click', () => {
+                    this.onClick()
+                })
+            } else if (this.trigger === 'hover') {
+                this.$refs.popover.addEventListener('mouseenter', this.onHoverOpen)
+                this.$refs.popover.addEventListener('mouseleave', this.onHoverClose)
+            } else if (this.trigger === 'focus') {
+                this.$refs.trigger.addEventListener('mousedown', () => {
+                    this.visible = true
+                })
+                this.$refs.trigger.addEventListener('mouseup', () => {
+                    this.visible = false
+                })
+            }
+
 
         },
         methods: {
             onClick() {
-                this.visible = !this.visible
+                if (this.visible !== true) {
+                    this.open()
+                } else {
+                    this.close()
+                }
+            },
+            open() {
+                this.visible = true
+                this.$nextTick(() => {
+                    document.body.addEventListener('click', this.onDocumentClick)
+                })
+            },
+            close() {
+                this.visible = false
+                document.body.removeEventListener('click', this.onDocumentClick)
+            },
+            onDocumentClick(e) {
+                let {trigger, content, popover} = this.$refs
+                if (this.$refs && (trigger.contains(e.target) || trigger === e.target)) {
+                    console.log('trigger click')
+                    return
+                } else if (this.$refs && (content === e.target || trigger.contains(e.target))) {
+                    console.log('content click')
+                    return
+                }
+                this.close()
+                console.log('document click')
+            },
+            onHoverOpen() {
+                console.log(1)
+                this.visible = true
+            },
+            onHoverClose() {
+                this.visible = false
             }
+
         }
     }
 </script>
