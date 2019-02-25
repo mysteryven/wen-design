88d6b80b9d2a9e56fa0f544ca10aba576909d74c
wenzhe <mysteryven@gmail.com>
2019-02-25 21:37:43 +0800

    重构 clickoutside 的代码

--- src/click-outside.js
+++ src/click-outside.js
@@ -1,17 +1,30 @@
-document.addEventListener('click', (e) => {
-   callbacks.forEach(item => {
-      let {el, value} = item
-      if (e.target === el || el.contains(e.target)) {
-           return
-      } else {
-           value()
-      }
-   })
-})
+document.addEventListener('click', listenToClick)
+
+function listenToClick(e) {
+    callbacks.forEach(item => {
+        let {el, value} = item
+        if (e.target === el || el.contains(e.target)) {
+            return
+        } else {
+            value()
+        }
+    })
+}
 
 let callbacks = []
 export default {
     bind(el, binding) {
         callbacks.push({el, value: binding.value})
+    },
+    unbind(el) {
+        callbacks.forEach((item, index) => {
+            if (el === item.el || el.contains(item.el)) {
+                callbacks.splice(index, 1)
+                console.log(callbacks)
+                document.removeEventListener('click', listenToClick)
+                document.addEventListener('click', listenToClick)
+                return;
+            }
+        })
     }
 }
