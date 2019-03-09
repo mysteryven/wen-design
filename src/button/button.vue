3d348b4692cc0a17a9c3d71b37358671b0918ca8
wenzhe <mysteryven@gmail.com>
2019-03-09 20:25:46 +0800

    添加 button 的测试用例

--- src/button/button.vue
+++ src/button/button.vue
@@ -42,6 +42,7 @@
                 type: Boolean,
                 default: false
             }
+
         },
         mounted() {
             this.$el.addEventListener('animationend', this.listenAnimation)
--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -17,7 +17,15 @@ describe('Button', () => {
         const useElement = wrapper.find('use')
         expect(useElement.attributes()['href']).to.equal('#icon-setting')
     })
-
+    it('可以设置iconPosition.', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'setting',
+                iconPosition: 'right'
+            }
+        })
+        expect(wrapper.classes()).contain('icon-right')
+    })
     it('可以设置loading.', () => {
         const wrapper = mount(Button, {
             propsData: {
@@ -30,14 +38,30 @@ describe('Button', () => {
         expect(useElements.length).to.equal(1)
         expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
     })
-
+    it('可以设置为 disabled.', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'setting',
+                disabled: true
+            }
+        })
+        expect(wrapper.classes()).contain('disabled')
+    })
+    it('可以设置为圆形.', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'setting',
+                isCircle: true
+            }
+        })
+        expect(wrapper.classes()).contain('circle')
+    })
     it('点击 button 触发 click 事件', () => {
         const wrapper = mount(Button, {
             propsData: {
                 icon: 'setting',
             }
         })
-
         const vm = wrapper.vm
         const callback = sinon.fake();
         vm.$on('click', callback)
